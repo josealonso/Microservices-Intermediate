c03c22be1d9a45a5dd66c9e1058ace888e427a7a
josealonso <jose.alonso.programmer@gmail.com>
2023-02-01 18:47:39 +0100

    Created Department JPA entity and Data JPA repository.

---
+++ department-service/src/main/java/info/josealonso/departmentservice/repository/DepartmentRepository.java
@@ -0,0 +1,7 @@
+package info.josealonso.departmentservice.repository;
+
+import info.josealonso.departmentservice.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
