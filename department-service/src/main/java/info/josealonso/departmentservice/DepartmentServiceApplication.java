a34d90d481f2eb042db61d660cb6af0f942131f1
josealonso <jose.alonso.programmer@gmail.com>
2023-02-02 18:27:47 +0100

    Registered Eureka clients.

--- department-service/src/main/java/info/josealonso/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/info/josealonso/departmentservice/DepartmentServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableEurekaClient   // this annotation no longer exists
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
