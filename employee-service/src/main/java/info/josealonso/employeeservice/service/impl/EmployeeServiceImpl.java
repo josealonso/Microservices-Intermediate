527e4d9966c62a7e0f1f6eb0330f971fce1b4e50
josealonso <jose.alonso.programmer@gmail.com>
2023-02-04 17:23:58 +0100

    Implemented retry pattern.

--- employee-service/src/main/java/info/josealonso/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/info/josealonso/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,7 +8,10 @@ import info.josealonso.employeeservice.repository.EmployeeRepository;
 import info.josealonso.employeeservice.service.APIClient;
 import info.josealonso.employeeservice.service.EmployeeService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -17,8 +20,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 @AllArgsConstructor
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-
     private static final String DEPARTMENT_URL = "http:localhost:8080/api/departments/";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private final EmployeeRepository employeeRepository;
     // private final RestTemplate restTemplate;
     private final WebClient webClient;
@@ -31,9 +35,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         return convertEmployeeToEmployeeDto(savedEmployee);
     }
 
-    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getEmployeeById(Long employeeId) {
+        LOGGER.info("inside getEmployeeById method");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow();
 
       /*  ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(DEPARTMENT_URL + employee.getDepartmentCode(),
@@ -58,6 +64,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+        LOGGER.info("inside getDefaultDepartment method");
         Employee employee = employeeRepository.findById(employeeId).orElseThrow();
 
         DepartmentDto departmentDto = DepartmentDto.builder()
